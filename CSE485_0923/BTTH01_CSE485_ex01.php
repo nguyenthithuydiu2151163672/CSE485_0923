e2378cee4be8b12daacd30d3a7e2b2186eaab37a
nguyenthithuydiu2151163672 <145408200+nguyenthithuydiu2151163672@users.noreply.github.com>
2023-09-25 17:33:39 +0700

    Add files via upload

--- CSE485_0923/BTTH01_CSE485_ex01.php
+++ CSE485_0923/BTTH01_CSE485_ex01.php
@@ -1,13 +1,3 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
 <?php
 function tinhMang($arrs) {
     // Tính tổng
@@ -45,6 +35,3 @@ function tinhMang($arrs) {
 $arrs = [2, 5, 6, 9, 2, 5, 6, 12, 5];
 tinhMang($arrs);
 ?>
-
-</body>
-</html>
